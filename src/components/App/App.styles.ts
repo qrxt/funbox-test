bc937ee86d5a1e30c1d34d5dbc3f9f454aeff429
qrxt <qrx357@gmail.com>
2023-01-18 22:29:38 +0300

    Fix fonts loading

--- src/components/App/App.styles.ts
+++ src/components/App/App.styles.ts
@@ -15,19 +15,20 @@ export const globalStyles = css`
     font-family: "Exo 2";
     font-style: normal;
     font-weight: 100;
-    src: url("src/assets/fonts/exo-2-v20-latin_cyrillic-100.eot"); /* IE9 Compat Modes */
+    src: url("/src/assets/fonts/exo-2-v20-latin_cyrillic-100.eot"); /* IE9 Compat Modes */
     src: local(""),
-      url("src/assets/fonts/exo-2-v20-latin_cyrillic-100.eot?#iefix")
+      url("/src/assets/fonts/exo-2-v20-latin_cyrillic-100.eot?#iefix")
         format("embedded-opentype"),
-      /* IE6-IE8 */ url("src/assets/fonts/exo-2-v20-latin_cyrillic-100.woff2")
+      /* IE6-IE8 */ url("/src/assets/fonts/exo-2-v20-latin_cyrillic-100.woff2")
         format("woff2"),
       /* Super Modern Browsers */
-        url("src/assets/fonts/exo-2-v20-latin_cyrillic-100.woff") format("woff"),
+        url("/src/assets/fonts/exo-2-v20-latin_cyrillic-100.woff")
+        format("woff"),
       /* Modern Browsers */
-        url("src/assets/fonts/exo-2-v20-latin_cyrillic-100.ttf")
+        url("/src/assets/fonts/exo-2-v20-latin_cyrillic-100.ttf")
         format("truetype"),
       /* Safari, Android, iOS */
-        url("src/assets/fonts/exo-2-v20-latin_cyrillic-100.svg#Exo2")
+        url("/src/assets/fonts/exo-2-v20-latin_cyrillic-100.svg#Exo2")
         format("svg"); /* Legacy iOS */
   }
 
