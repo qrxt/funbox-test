f9f150a10aa4edb2d06f6af7905527dae5ac4039
qrxt <qrx357@gmail.com>
2023-01-18 22:50:07 +0300

    Fix fonts

--- src/components/App/App.styles.ts
+++ src/components/App/App.styles.ts
@@ -3,6 +3,11 @@ import emotionReset from "emotion-reset";
 import theme from "lib/theme";
 // TODO: alias "assets/..."
 import patternImage from "../../assets/images/patternmin.png";
+import exoFontWoff2 from "../../assets/fonts/exo-2-v20-latin_cyrillic-100.woff2";
+import exoFontWoff from "../../assets/fonts/exo-2-v20-latin_cyrillic-100.woff";
+import exoFontEot from "../../assets/fonts/exo-2-v20-latin_cyrillic-100.eot";
+import exoFontTtf from "../../assets/fonts/exo-2-v20-latin_cyrillic-100.ttf";
+import exoFontSvg from "../../assets/fonts/exo-2-v20-latin_cyrillic-100.svg";
 
 export const globalStyles = css`
   ${emotionReset}
@@ -15,21 +20,10 @@ export const globalStyles = css`
     font-family: "Exo 2";
     font-style: normal;
     font-weight: 100;
-    src: url("/src/assets/fonts/exo-2-v20-latin_cyrillic-100.eot"); /* IE9 Compat Modes */
-    src: local(""),
-      url("/src/assets/fonts/exo-2-v20-latin_cyrillic-100.eot?#iefix")
-        format("embedded-opentype"),
-      /* IE6-IE8 */ url("/src/assets/fonts/exo-2-v20-latin_cyrillic-100.woff2")
-        format("woff2"),
-      /* Super Modern Browsers */
-        url("/src/assets/fonts/exo-2-v20-latin_cyrillic-100.woff")
-        format("woff"),
-      /* Modern Browsers */
-        url("/src/assets/fonts/exo-2-v20-latin_cyrillic-100.ttf")
-        format("truetype"),
-      /* Safari, Android, iOS */
-        url("/src/assets/fonts/exo-2-v20-latin_cyrillic-100.svg#Exo2")
-        format("svg"); /* Legacy iOS */
+    src: url(${exoFontEot}); /* IE9 Compat Modes */
+    src: local(""), url(${exoFontTtf}) format("embedded-opentype"),
+      url(${exoFontWoff2}) format("woff2"), url(${exoFontWoff}) format("woff"),
+      url(${exoFontTtf}) format("truetype"), url(${exoFontSvg}) format("svg");
   }
 
   html,
